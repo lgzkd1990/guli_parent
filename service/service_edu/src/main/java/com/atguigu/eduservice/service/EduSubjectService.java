992e9437fd50fbd953ebdb0660a30dd6207cab90
root <844923658@qq.com>
2020-05-05 18:41:36 +0800

    上传分类

--- service/service_edu/src/main/java/com/atguigu/eduservice/service/EduSubjectService.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduSubjectService.java
@@ -1,8 +1,11 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.entity.subject.OneSubject;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,4 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 public interface EduSubjectService extends IService<EduSubject> {
     //添加课程分类
     void saveSubject(MultipartFile file, EduSubjectService subjectService);
+
+    //课程分类列表（树形）
+    List<OneSubject> getAllOneTwoSubject();
 }
